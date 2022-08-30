857f5157355bc2df686e54fc087a6bf921a840c2
Waseemalame <waseemalame45@gmail.com>
2022-08-30 15:29:22 -0700

    post/comment model files created

---
+++ app/models/comment.py
@@ -0,0 +1,24 @@
+from turtle import back
+from .db import db
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment_content = db.Column(db.String(2200), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
+    likes = db.Column(db.Integer, default=0)
+
+    post = db.relationship("Post", back_populates="comments")
+    user = db.relationship("User", back_populates="comments")
+    like_list = db.relationship("Like", back_populates="comment", cascade="all, delete-orphan")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "comment_content": self.comment_content,
+            "user": self.user.to_dict(),
+            "post": self.post_id,
+            "likes": self.likes
+        }
---
+++ app/models/post.py
@@ -0,0 +1,37 @@
+from .db import db
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(2200), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    likes = db.Column(db.Integer, default=0)
+    image_url = db.Column(db.String, nullable=False)
+    display_comments = db.Column(db.Boolean, default=True)
+
+    user = db.relationship("User", back_populates="posts")
+    comments = db.relationship("Comment", back_populates="post", cascade="all, delete-orphan")
+    like_list = db.relationship("Like", back_populates="post", cascade="all, delete-orphan")
+
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "user": self.user.to_dict(),
+            "likes": self.likes,
+            "like_list": [like.to_dict() for like in self.like_list],
+            "image_url": self.image_url,
+            "comments": [comment.to_dict() for comment in self.comments],
+            "display_comments": self.display_comments
+        }
+
+    def __repr__(self):
+        return f"""
+            < Post ID: {self.id}\n
+              content: {self.content}\n
+              User: {self.user.to_dict()}\n
+              Likes: {self.likes}\n
+              Image URL: {self.image_url} >
+            """
