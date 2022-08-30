32d961328df21582cbfe5ad8cea3e5082b6e1a3e
Waseemalame <waseemalame45@gmail.com>
2022-08-30 15:33:38 -0700

    SideBar created

---
+++ react-app/src/components/Feed/SideBar/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './SideBar.css'
+const SideBar = () => {
+  return (
+    <div className='sidebar-container'>
+        <div><img className='profile-icon' src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.30497-1/143086968_2856368904622192_1959732218791162458_n.png?stp=cp0_dst-png_p60x60&_nc_cat=1&ccb=1-7&_nc_sid=7206a8&_nc_ohc=kfSdWSkKc8EAX_rxrhi&_nc_ht=scontent-lax3-1.xx&oh=00_AT-KWu2XuszBsIEBIWx2SaOfefmOKr1BSaqZIpgB_swV-w&oe=63217AF8" alt=""/>Waseem Alame</div>
+        <div><img src="https://static.xx.fbcdn.net/rsrc.php/v3/y8/r/S0U5ECzYUSu.png" alt=""/>Friends</div>
+        <div><img src="https://static.xx.fbcdn.net/rsrc.php/v3/yU/r/D2y-jJ2C_hO.png" alt=""/>Marketplace</div>
+        <div><img src="https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/PrjLkDYpYbH.png" alt=""/>Groups</div>
+        <div><img src="https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/duk32h44Y31.png" alt=""/>Watch</div>
+        <div><img src="https://static.xx.fbcdn.net/rsrc.php/v3/y8/r/he-BkogidIc.png" alt=""/>Memories</div>
+        <div><img src="https://static.xx.fbcdn.net/rsrc.php/v3/yD/r/lVijPkTeN-r.png" alt=""/>Saved</div>
+    </div>
+  )
+}
+
+export default SideBar
