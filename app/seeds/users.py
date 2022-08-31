c00a3dd42182791983f93aa130f64c2374c841b3
Waseemalame <waseemalame45@gmail.com>
2022-08-30 17:15:36 -0700

    added user columns

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,11 +4,11 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
+        username='Demo', email='demo@aa.io', password='password', first_name='sammy', last_name='tommy', profile_image_url="https://avatars.githubusercontent.com/u/73668892?v=4")
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
+        username='marnie', email='marnie@aa.io', password='password', first_name='sammy', last_name='tommy', profile_image_url="https://avatars.githubusercontent.com/u/73668892?v=4")
     bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        username='bobbie', email='bobbie@aa.io', password='password', first_name='sammy', last_name='tommy', profile_image_url="https://avatars.githubusercontent.com/u/73668892?v=4")
 
     db.session.add(demo)
     db.session.add(marnie)
