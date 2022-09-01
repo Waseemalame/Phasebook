aad9b54fdbd3fce650c4612de7774a0b72ae5123
Waseemalame <waseemalame45@gmail.com>
2022-08-31 22:00:27 -0700

    created comment model and relationships

--- app/models/comment.py
+++ app/models/comment.py
@@ -8,11 +8,11 @@ class Comment(db.Model):
     comment_content = db.Column(db.String(2200), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
-    likes = db.Column(db.Integer, default=0)
+    # likes = db.Column(db.Integer, default=0)
 
     post = db.relationship("Post", back_populates="comments")
     user = db.relationship("User", back_populates="comments")
-    like_list = db.relationship("Like", back_populates="comment", cascade="all, delete-orphan")
+    # like_list = db.relationship("Like", back_populates="comment", cascade="all, delete-orphan")
 
     def to_dict(self):
         return {
@@ -20,5 +20,5 @@ class Comment(db.Model):
             "comment_content": self.comment_content,
             "user": self.user.to_dict(),
             "post": self.post_id,
-            "likes": self.likes
+            # "likes": self.likes
         }
