9e43af2668f8bab4abb67f09b8f3385a28a342e5
Waseemalame <waseemalame45@gmail.com>
2022-08-31 22:02:29 -0700

    created comment form component

---
+++ react-app/src/components/CreateComment/CreateCommentForm.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { createComment, getComments } from '../../store/comment';
+import { getPostsThunk } from '../../store/post';
+import './CreateCommentForm.css';
+
+const CreateCommentForm = ({ post }) => {
+const [commentContent, setCommentContent] = useState('');
+const [errorValidators, setErrorValidators] = useState([]);
+const updateComment = (e) => setCommentContent(e.target.value);
+
+const dispatch = useDispatch()
+const userSession = useSelector(state => state.session.user)
+
+
+let errors = [];
+
+  const commentSubmit = async(e) => {
+    e.preventDefault();
+    const data = {
+      comment_content: commentContent,
+      post_id: post.id,
+      user_id: userSession.id
+    }
+
+    let newComment = await dispatch(createComment(data))
+    if(commentContent.length === 0){
+      errors.push('Your comment needs at least one character')
+      setErrorValidators(errors);
+    }
+
+    dispatch(getComments(post.id))
+    dispatch(getPostsThunk())
+
+    setCommentContent('')
+    return newComment
+  }
+
+  return (
+    <form className="comment-form" onSubmit={commentSubmit}>
+      <ul>{errorValidators.map(error =><li>{error}</li>)}</ul>
+        <input type="text"
+        className='comment-input'
+           value={commentContent}
+           onChange={updateComment}
+           placeholder='Add a comment'
+           required />
+    <button type="submit">Post</button>
+  </form>
+  )
+}
+
+export default CreateCommentForm
