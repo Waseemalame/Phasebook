9f4ef24fde999bfeb8e88582467897c4b0452e7e
Waseemalame <waseemalame45@gmail.com>
2022-09-02 08:55:42 -0700

    edit comment route created

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -14,3 +14,24 @@ def all_comments():
     return response
   else:
     return "Unauthorized"
+
+@comment_routes.route("/<comment_id>", methods=["PUT"])
+def edit_comment(comment_id):
+
+    comment = Comment.query.get(comment_id)
+
+    updated_comment = CommentForm()
+
+    updated_comment['csrf_token'].data = request.cookies['csrf_token']
+    comment_content = updated_comment.data["comment_content"]
+    user_id = updated_comment.data["user_id"]
+    post_id = updated_comment.data["post_id"]
+
+    if current_user.id == comment.user_id:
+        comment.comment_content = comment_content
+        comment.user_id = user_id
+        comment.post_id = post_id
+        db.session.commit()
+        return comment.to_dict()
+    else:
+         return '404: unauthorized user'
