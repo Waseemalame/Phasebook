526b536e011431571026c9e744f0bc8235a4bba5
Waseemalame <waseemalame45@gmail.com>
2022-09-03 22:32:17 -0700

    changed marnie image

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -6,7 +6,7 @@ def seed_users():
     demo = User(
         username='Demo', email='demo@aa.io', password='password', first_name='Demo', last_name='User', profile_image_url="https://avatars.githubusercontent.com/u/73668892?v=4")
     marnie = User(
-        username='marnie', email='marnie@aa.io', password='password', first_name='marnie', last_name='ann', profile_image_url="https://avatars.githubusercontent.com/u/73668892?v=4")
+        username='marnie', email='marnie@aa.io', password='password', first_name='marnie', last_name='ann', profile_image_url="https://images.unsplash.com/photo-1507019403270-cca502add9f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z2lybCUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80")
     bobbie = User(
         username='bobbie', email='bobbie@aa.io', password='password', first_name='bobby', last_name='willin', profile_image_url="https://d2qp0siotla746.cloudfront.net/img/use-cases/profile-picture/template_0.jpg")
 
