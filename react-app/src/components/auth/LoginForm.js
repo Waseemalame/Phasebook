d21ae178061d9d9aaca067500acd2ba868533a64
Waseemalame <waseemalame45@gmail.com>
2022-09-09 12:29:02 -0700

    error validations styled

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { NavLink, Redirect } from 'react-router-dom';
 import { demoLogin, login } from '../../store/session';
@@ -11,6 +12,16 @@ const LoginForm = () => {
   const [password, setPassword] = useState('');
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    const signupBtn = document.querySelector(".signup-div")
+    console.log(signupBtn)
+    if(!errors.length > 0){
+      signupBtn.style.marginTop = '50px'
+    } else {
+      signupBtn.style.marginTop = '20px'
+    }
+  }, [errors]);
 
   const onLogin = async (e) => {
     e.preventDefault();
@@ -35,10 +46,18 @@ const LoginForm = () => {
     dispatch(demoLogin())
   }
 
+
   return (
     <div>
       <div className="fb-header-div"><img className='fb-header-img' src="https://i.imgur.com/HwGVt4u.png" alt="" /></div>
       <form className='login-form' onSubmit={onLogin}>
+      {errors.length > 0 && (
+                  <div className='login-errors'>
+                  {errors.map((error, ind) => (
+                    <div key={ind}>{error}</div>
+                  ))}
+                </div>
+          )}
         <div className='email-div'>
           {/* <label htmlFor='email'>Email</label> */}
           <input
@@ -60,13 +79,6 @@ const LoginForm = () => {
             />
         </div>
           <button className='login-submit' type='submit'>Log In</button>
-          {errors.length > 0 && (
-                  <div>
-                  {errors.map((error, ind) => (
-                    <div key={ind}>{error}</div>
-                  ))}
-                </div>
-          )}
         <p className='or-text'>Or</p>
         <button onClick={handleDemoLogin} className='login-submit' type='button'>Demo Login</button>
         <SignUpFormModal />
