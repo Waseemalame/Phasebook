7191ad407c2dcd2df906e8c7c3f59e690f10ff39
Waseemalame <waseemalame45@gmail.com>
2022-09-09 11:55:39 -0700

    delete post modal complete

--- react-app/src/components/DeletePost/DeletePost.js
+++ react-app/src/components/DeletePost/DeletePost.js
@@ -2,16 +2,31 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { deletePostThunk } from '../../store/post'
 
-const DeletePost = ({ post }) => {
+const DeletePost = ({ post, setShowDeleteModal, setShowPostOptionsModal }) => {
   const dispatch = useDispatch();
   const handleDeletePost = () => {
     dispatch(deletePostThunk(post.id))
   }
   return (
     <div>
-      Are you sure you want to delete this post?
-      <div onClick={handleDeletePost}>Yes</div>
-      <div>No</div>
+      {/* Are you sure you want to delete this post? */}
+      {/* <div onClick={handleDeletePost}>Yes</div>
+      <div onClick={() => setShowDeleteModal(false)}>No</div> */}
+      <div className="confirm-del-container">
+        <div className="del-header">
+        <h3>Delete Post?</h3>
+      </div>
+    <p className="del-text">Are you sure you want to delete this post?</p>
+    <div className="delete-options">
+      <h4 className="no-delete" onClick={
+        () => {
+          setShowPostOptionsModal(false)
+          setShowDeleteModal(false)
+        }
+      }>No</h4>
+      <h4 className="yes-delete" onClick={handleDeletePost}>Delete</h4>
+    </div>
+    </div>
     </div>
   )
 }
