07d83b02098b6ab761504cb14de8e9be864293f2
Waseemalame <waseemalame45@gmail.com>
2022-09-12 00:30:38 -0700

    seed data added

--- app/seeds/posts.py
+++ app/seeds/posts.py
@@ -13,6 +13,19 @@ def seed_posts():
         content='“We are what we repeatedly do. Excellence, then, is not an act, but a habit” - Aristotle', user_id=1)
     demo5 = Post(
         content="I want to travel can someone help me decide where to go first?", user_id=2)
+    demo6 = Post(
+        content="I want this home!", user_id=4)
+    demo7 = Post(
+        content="I'm a professional photographer if anyone is interested!", user_id=5)
+    demo8 = Post(
+        content="Looking into buying Phasedbook soon if the developer accepts Doge Coin", user_id=6)
+    demo9 = Post(
+        content="I'm fascinated by how fast technology is progressing!.", user_id=3)
+    demo10 = Post(
+        content="If anyone has any puppies I'd love to take them off your hands!", user_id=2)
+
+
+
 
 
     db.session.add(demo1)
@@ -20,6 +33,11 @@ def seed_posts():
     db.session.add(demo3)
     db.session.add(demo4)
     db.session.add(demo5)
+    db.session.add(demo6)
+    db.session.add(demo7)
+    db.session.add(demo8)
+    db.session.add(demo9)
+    db.session.add(demo10)
 
     db.session.commit()
 
