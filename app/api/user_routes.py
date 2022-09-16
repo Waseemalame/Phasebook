623099ba289423dff2c81e5757f6cb841ec30647
Waseemalame <waseemalame45@gmail.com>
2022-09-16 12:25:14 -0700

    get mutual friends route created

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
 from app.models import User
+from flask_login import current_user
+
 
 user_routes = Blueprint('users', __name__)
 
@@ -10,6 +12,19 @@ user_routes = Blueprint('users', __name__)
 def users():
     users = User.query.all()
     return {'users': [user.to_dict() for user in users]}
+
+@user_routes.route('/mutualfriends/<user_id>')
+@login_required
+def user_friends(user_id):
+    user = User.query.get(user_id)
+
+    friend_arr = []
+    for friend in current_user.friends:
+        for user_friend in user.friends:
+            if friend.to_dict() == user_friend.to_dict():
+                print(friend.to_dict())
+                friend_arr.append(friend)
+    return {'users': [user.to_dict() for user in friend_arr]}
 
 
 @user_routes.route('/<int:id>')
