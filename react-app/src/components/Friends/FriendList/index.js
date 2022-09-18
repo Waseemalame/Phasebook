05f1700af1f9b0ae14bc4887574ec307f0536d5a
Waseemalame <waseemalame45@gmail.com>
2022-09-17 19:20:52 -0700

    new friend list component created

---
+++ react-app/src/components/Friends/FriendList/index.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { getAllFriendsThunk } from '../../../store/request'
+import OneFriend from '../OneFriend'
+import "./FriendList.css"
+const FriendsList = ({ user, mutualFriends, accepted, deleted, setAccepted, setDeleted }) => {
+  const current_user = useSelector(state => state.session.user)
+  const [friends, setFriends] = useState([]);
+
+  // const { userId }  = useParams();
+  const userId = user.id
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+
+    (async () => {
+      const response = await fetch(`/api/users/${userId}/friends`);
+
+      const users_friends = await response.json();
+      setFriends(users_friends.friends);
+    })();
+    // dispatch(getAllFriendsThunk())
+    if(deleted === true){
+      setDeleted(false)
+    }
+    if(accepted === true){
+      setAccepted(false)
+    }
+  }, [userId, user, accepted, deleted]);
+
+
+
+  return (
+    <>
+      <div className="profile-friends-info">
+        <div className='friends-info-top'>
+          <h3 className='friend-header'>Friends</h3>
+          <p className='see-all-friends'>{friends.length > 8 && 'See all friends'}</p>
+        </div>
+        <span className='friends-amount'>{friends.length} {current_user.id !== userId ? (
+          `(${mutualFriends.length} mutual)`
+        ) : 'friends'} </span>
+      </div>
+      <div className='friends-list'>
+        {friends && friends.map((friend, i) => (
+          <>
+            {i > 8 ? '' : (
+              <OneFriend friend={friend}/>
+            )}
+          </>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default FriendsList
