19e66c89b1164f86bf8a24f9244e21b44427ddc6
Waseemalame <waseemalame45@gmail.com>
2022-09-21 17:45:08 -0700

    close msg pop up on logout

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { useMessageContext } from '../../context/messageContext';
 import { logout } from '../../store/session';
 import "./authStyles/LogoutButton.css"
+
 const LogoutButton = () => {
+  const { showMsgPopup, setShowMsgPopup, msgUser, setMsgUser } = useMessageContext()
   const dispatch = useDispatch()
   const onLogout = async (e) => {
     await dispatch(logout());
+    setShowMsgPopup(false)
+    setMsgUser(null)
   };
 
   return <button className='logout-btn' onClick={onLogout}>Logout</button>;
