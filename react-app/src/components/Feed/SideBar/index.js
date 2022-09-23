cb454745f77130fa5e9d23843d6c7c46f0a5c4ce
Waseemalame <waseemalame45@gmail.com>
2022-09-22 19:11:00 -0700

    styles and redirect function added

--- react-app/src/components/Feed/SideBar/index.js
+++ react-app/src/components/Feed/SideBar/index.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 import './SideBar.css'
-
 const SideBar = () => {
+
   const current_user = useSelector(state => state.session.user)
+  const history = useHistory()
+
+  const redirectProfile = (user) => {
+    history.push(`/users/${user.id}`)
+  }
 
   return (
     <div className='sidebar-container'>
-        <div><img className='profile-icon' src={current_user.profile_image_url} alt=""/>Waseem Alame</div>
+        <div className='sidebar-to-profile' onClick={() => redirectProfile(current_user)}><img className='profile-icon' src={current_user.profile_image_url} alt=""/>{current_user.first_name} {current_user.last_name}</div>
         <div><img src="https://static.xx.fbcdn.net/rsrc.php/v3/y8/r/S0U5ECzYUSu.png" alt=""/>Friends</div>
         <div><img src="https://static.xx.fbcdn.net/rsrc.php/v3/yU/r/D2y-jJ2C_hO.png" alt=""/>Marketplace</div>
         <div><img src="https://static.xx.fbcdn.net/rsrc.php/v3/y5/r/PrjLkDYpYbH.png" alt=""/>Groups</div>
