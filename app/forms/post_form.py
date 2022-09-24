31e97ce54688aaeffa30fb9a373a9924cc2ee0a6
Waseemalame <waseemalame45@gmail.com>
2022-09-23 19:54:05 -0700

    timestamp for posts added

--- app/forms/post_form.py
+++ app/forms/post_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, BooleanField
+from wtforms import StringField, IntegerField, BooleanField, DateTimeField
 from wtforms.validators import DataRequired
 
 # img url, userId, content
@@ -8,3 +8,5 @@ class PostForm(FlaskForm):
   user_id = IntegerField("User_id", validators=[DataRequired()])
   image_url = StringField("Image_url")
   content = StringField("Content", validators=[DataRequired()])
+  created_at = DateTimeField('created_at', format="%Y-%m-%d %H:%M:%S")
+  updated_at = DateTimeField('updated_at', format="%Y-%m-%d %H:%M:%S")
