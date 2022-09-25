34f885c33eeb2c1cf4819ea08e5e3d8cd3fd7afd
Waseemalame <waseemalame45@gmail.com>
2022-09-24 20:35:33 -0700

    POST and DELETE thunks for Likes complete

--- react-app/src/store/like.js
+++ react-app/src/store/like.js
@@ -6,11 +6,44 @@ const getLikes = (likes) => ({
   type: GET_LIKES,
   likes
 })
+const addLike = (like) => ({
+  type: ADD_LIKE,
+  like
+})
+const deleteLike = (likeId) => ({
+  type: DELETE_LIKE,
+  likeId
+})
 export const getLikesThunk = () => async (dispatch) => {
   const res = await fetch("/api/likes")
   if(res.ok){
     const likes = await res.json()
     dispatch(getLikes(likes.likes))
+  }
+}
+export const addLikeThunk = (data) => async (dispatch) => {
+  const { post_id, user_id } = data
+  const formData = new FormData()
+  formData.append('post_id', post_id)
+  formData.append('user_id', user_id)
+
+  const res = await fetch(`/api/likes/`, {
+    method: 'POST',
+    body: formData,
+  })
+  if(res.ok){
+    const like = await res.json()
+    await dispatch(addLike(like))
+    return like
+  }
+}
+
+export const deleteLikeThunk = (likeId) => async (dispatch) => {
+  const res = await fetch(`/api/likes/${likeId}`, {
+    method: 'DELETE'
+  })
+  if(res.ok){
+    await dispatch(deleteLike(likeId))
   }
 }
 
@@ -24,6 +57,15 @@ const likesReducer = (state = initialState, action) => {
         allLikes[like.id] = like
       })
       return allLikes
+    case ADD_LIKE:
+      return {
+        ...state,
+        [action.like.id]: action.like
+      }
+    case DELETE_LIKE:
+      const likeState = { ...state }
+      delete likeState[action.likeId]
+      return likeState
     default:
       return state;
   }
