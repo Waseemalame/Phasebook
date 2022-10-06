401150381b12600e9d149e0c64bccf1bfc216a47
Waseemalame <waseemalame45@gmail.com>
2022-10-06 10:21:43 -0700

    moved most recent message to top of messages

--- react-app/src/components/Contacts/Contacts.js
+++ react-app/src/components/Contacts/Contacts.js
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, useHistory } from 'react-router-dom';
+import { loadMessagesThunk } from '../../store/message';
 import { getAllFriendsThunk, getAllRequestsThunk } from '../../store/request';
 import { useMessageContext } from '../context/messageContext';
-// import "../RightSideBar/RightSideBar.css"
+import OneContact from './OneContact';
 function Contacts({ searchString, setSearchList }) {
   const [users, setUsers] = useState([]);
   const [mutualFriends, setMutualFriends] = useState([]);
@@ -12,8 +13,30 @@ function Contacts({ searchString, setSearchList }) {
   const history = useHistory()
   const current_user = useSelector(state => state.session.user)
   const friends = useSelector(state => Object.values(state.friends))
-  const { showMsgPopup, setShowMsgPopup, msgUser, setMsgUser, messages, setMessages } = useMessageContext()
+  const { showMsgPopup, setShowMsgPopup, msgUser, setMsgUser, setMessages, mostRecentMessager } = useMessageContext()
+
   const dispatch = useDispatch()
+  const [sortedFriends, setSortedFriends] = useState([]);
+
+  useEffect(() => {
+    dispatch(loadMessagesThunk())
+  }, []);
+
+  useEffect(() => {
+
+    let index;
+    for (let i = 0; i < friends.length; i++) {
+      const friend = friends[i];
+      if(friend.id === mostRecentMessager){
+        index = i
+      }
+    }
+
+    const friendAtIndex = friends[index]
+    friends.splice(index, 1)
+    friends.unshift(friendAtIndex)
+    setSortedFriends(friends)
+  }, [mostRecentMessager]);
 
   useEffect(() => {
     async function fetchData() {
@@ -29,29 +52,6 @@ useEffect(() => {
 }, [dispatch]);
 
 
-
-  const findMutualFriends = async (e, friendId) => {
-    e.stopPropagation()
-    const response = await fetch(`/api/users/mutualfriends/${friendId}`);
-    const responseData = await response.json();
-    setMutualFriends(responseData.users);
-  }
-
-  const userComponents = users.map((user) => {
-    return (
-      <li key={user.id} className="user-list-item">
-        <NavLink to={`/users/${user.id}`}>{user.username}</NavLink>
-      </li>
-    );
-  });
-  const userSearchRedirect = (userId) => {
-    setSearchList(false)
-    history.push(`/users/${userId}`)
-
-  }
-  const redirectProfile = (user) => {
-    history.push(`/users/${user.id}`)
-  }
   const messagePopup = (user) => {
     if(user.id === current_user.id){
       setShowMsgPopup(false)
@@ -64,15 +64,11 @@ useEffect(() => {
       setMessages([])
     } else
     if(showMsgPopup){
-      // return;
       setShowMsgPopup(false)
       setMsgUser(null)
       setShowMsgPopup(true)
       setMsgUser(user)
       setMessages([])
-
-
-      // return
     }
       setShowMsgPopup(true)
       setMsgUser(user)
@@ -82,20 +78,10 @@ useEffect(() => {
   return (
 
       <div className='contacts-container'>
-          {friends && friends.map(friend => (
-                            <div onClick={() => messagePopup(friend)} className='contact'>
-                              <div>
-                                {friend.profile_image_url ? (
-                                  <img className="contact-image" src={friend.profile_image_url} alt="" />
-
-                                  ) : (
-
-                                  <img className="contact-image" src="https://i.imgur.com/hrQWTvu.png" alt="" />
-                                )}
-                                </div>
-
-                              <div className='contact-name'>{friend.first_name} {friend.last_name}</div>
-                            </div>
+          {sortedFriends && sortedFriends.map((friend, i) => (
+                <div key={`friends-messenger-${i}`} className="contact-wrapper">
+                  <OneContact friend={friend} messagePopup={messagePopup}/>
+                </div>
           ))}
       </div>
 
